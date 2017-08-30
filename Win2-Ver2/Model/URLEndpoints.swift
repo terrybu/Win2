8d2d72c037c95a321b240d8312df7d5a080d245b
Terry Bu <terry.bu@verizonwireless.com>
2017-08-30 17:13:50 -0400

    Facebook info pull working on device

--- Win2-Ver2/Model/URLEndpoints.swift
+++ Win2-Ver2/Model/URLEndpoints.swift
@@ -14,7 +14,7 @@ let kSocialServicesTag = "긍휼"
 
 let kImportIOURLForScrapingTenRecentWeeklyPrograms = "https://api.import.io/store/data/3bc15cdb-dbc3-4e1a-b937-df3c2a68dbbc/_query?input/webpage/url=http%3A%2F%2Fvision.onnuri.org%2Fin2%2Farchives%2Fsunday_bulletin_category%2Fsunday-bulletin&_user=0a668a36-6aa0-4bf7-a33f-4aa867422551&_apikey=0a668a366aa04bf7a33f4aa86742255106cd4c765c1dc526570f30d171b722431f993ad7303c4f09ce394851f9fc53aecb7bf49dd2f2f02cd8e0624fa8dedf188642ab734d5c9159f291501b1b381633"
 
-let kAppAccessToken = "424019454456359|1bKu9Z_qb5Ppyw5ysZ1-MSeh-So"
+let kAppAccessToken = "902871673195393|nGANcp0NnDuGozR7gDktHkadjiA"
 //this is the token for the entire app on Facebook Dashboard that I registered
 //using this token can allow you to easily get public open info like photos from a page
 
